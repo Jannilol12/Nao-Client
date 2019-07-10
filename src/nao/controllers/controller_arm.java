c78e5ade21a55ce5aa1f0d0c309cf29c921e01c1
BuildTools <unconfigured@null.spigotmc.org>
2019-07-10 08:44:56 +0200

    Add ButtonLongPress in controller_arm

--- src/nao/controllers/controller_arm.java
+++ src/nao/controllers/controller_arm.java
@@ -1,51 +1,76 @@
 package nao.controllers;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Slider;
-import javafx.scene.input.MouseEvent;
-import nao.sender;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class controller_arm {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import nao.sender;
+import nao.events.ButtonLongPress;
+
+public class controller_arm implements Initializable{
 	@FXML
     private Slider arms_speed;
 	
 	@FXML
-    void LShoulderPitch_Down(MouseEvent event) {
-        sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"LShoulderPitch_Down\", \"value\":" + arms_speed.getValue() +"}");
-    }
+	private Button LShoulderPitch_Down;
+	
+	@FXML
+	private Button LShoulderPitch_Up;
+	
+	@FXML
+	private Button LShoulderRoll_Left;
+	
+	@FXML
+	private Button LShoulderRoll_Right;
+	
+	@FXML
+	private Button RShoulderPitch_Down;
+	
+	@FXML
+	private Button RShoulderPitch_Up;
+	
+	@FXML
+	private Button RShoulderRoll_Left;
+	
+	@FXML
+	private Button RShoulderRoll_Right;
 
-    @FXML
-    void LShoulderPitch_Up(MouseEvent event) {
-    	sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"LShoulderPitch_Up\", \"value\":" + arms_speed.getValue() +"}");
-    }
-
-    @FXML
-    void LShoulderRoll_Left(MouseEvent event) {
-    	sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"LShoulderRoll_Left\", \"value\":" + arms_speed.getValue() +"}");
-    }
-
-    @FXML
-    void LShoulderRoll_Right(MouseEvent event) {
-    	sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"LShoulderRoll_Right\", \"value\":" + arms_speed.getValue() +"}");
-    }
-    
-    @FXML
-    void RShoulderPitch_Down(MouseEvent event) {
-        sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"RShoulderPitch_Down\", \"value\":" + arms_speed.getValue() +"}");
-    }
-
-    @FXML
-    void RShoulderPitch_Up(MouseEvent event) {
-    	sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"RShoulderPitch_Up\", \"value\":" + arms_speed.getValue() +"}");
-    }
-
-    @FXML
-    void RShoulderRoll_Left(MouseEvent event) {
-    	sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"RShoulderRoll_Left\", \"value\":" + arms_speed.getValue() +"}");
-    }
-
-    @FXML
-    void RShoulderRoll_Right(MouseEvent event) {
-    	sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"RShoulderRoll_Right\", \"value\":" + arms_speed.getValue() +"}");
-    }
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		new ButtonLongPress(LShoulderPitch_Down, () -> {
+			sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"LShoulderPitch_Down\", \"value\":" + arms_speed.getValue() +"}");
+		});
+		
+		new ButtonLongPress(LShoulderPitch_Up, () -> {
+			sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"LShoulderPitch_Up\", \"value\":" + arms_speed.getValue() +"}");
+		});
+		
+		new ButtonLongPress(LShoulderRoll_Left, () -> {
+			sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"LShoulderRoll_Left\", \"value\":" + arms_speed.getValue() +"}");
+		});
+		
+		new ButtonLongPress(LShoulderRoll_Right, () -> {
+			sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"LShoulderRoll_Right\", \"value\":" + arms_speed.getValue() +"}");
+		});
+		
+		new ButtonLongPress(RShoulderPitch_Up, () -> {
+			sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"RShoulderPitch_Up\", \"value\":" + arms_speed.getValue() +"}");
+		});
+		
+		new ButtonLongPress(LShoulderPitch_Down, () -> {
+			sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"LShoulderPitch_Down\", \"value\":" + arms_speed.getValue() +"}");
+		});
+		
+		new ButtonLongPress(RShoulderRoll_Left, () -> {
+			sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"RShoulderRoll_Left\", \"value\":" + arms_speed.getValue() +"}");
+		});
+		
+		new ButtonLongPress(RShoulderRoll_Right, () -> {
+			sender.sendMessage("{\"type\":\"motors\", \"motorname\":\"RShoulderRoll_Right\", \"value\":" + arms_speed.getValue() +"}");
+		});
+		
+	}
 }
--- src/nao/controllers/controller_leg.java
+++ src/nao/controllers/controller_leg.java
@@ -1,11 +1,19 @@
 package nao.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Slider;
 
-public class controller_leg {
+public class controller_leg implements Initializable{
 	@FXML
     private Slider leg_speed;
-	
-	
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 }
