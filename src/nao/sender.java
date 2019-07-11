2a1c1f1ce79d8e1c8ea4da506eb92aa04c11d6cf
BuildTools <unconfigured@null.spigotmc.org>
2019-07-11 06:13:26 +0200

    Should fix the Connection Status Icon (not always working)

--- src/nao/sender.java
+++ src/nao/sender.java
@@ -10,21 +10,25 @@ public class sender {
     public static Socket socket;
     public static DataOutputStream dout;
     public static DataInputStream dis;
+    public static boolean online;
+    
     public static String ip;
     public static int port;
     private static Thread readingThread;
 
-    public static void connected(String ip_,int Port_){
+    public synchronized static void connected(String ip_,int Port_){
         ip = ip_;
         port = Port_;
         reconnect();
     }
 
-    public static void reconnect(){
+    public synchronized static void reconnect(){
         destroy();
         try {
             socket = new Socket(ip , port);
+            online = true;
             socket.setKeepAlive(false);
+            
             dout = new DataOutputStream(socket.getOutputStream());
             dis = new DataInputStream(socket.getInputStream());
             startReadingThread();
@@ -40,9 +44,9 @@ public class sender {
     }
 
     public static void sendMessage(String text){
-        if(socket != null && socket.isClosed()){
+        if(isClosed())
             reconnect();
-        }
+        
         if(text != null && dout != null ) {
             try {
                 dout.writeUTF(text);
@@ -54,47 +58,48 @@ public class sender {
     }
 
     private synchronized static void startReadingThread(){
-        if(readingThread != null)return;
+        if(readingThread != null) return;
 
         readingThread = new Thread(){
             @Override
             public void run(){
-                while(!this.isInterrupted()){
+                while(!this.isInterrupted() && online){
                     try {
                         String str = dis.readUTF();
                         if(str.isEmpty())continue;
                         MainReceiver.receiveText(str);
                     } catch (IOException e) {
                         System.out.println("Stop reading...");
+                        destroy();
                         return;
                     }
                 }
+                
                 System.out.println("Stop reading...");
+                destroy();
             }
         };
         readingThread.start();
     }
 
-    public static void destroy(){
+    public synchronized static void destroy(){
+    	if(!online) return;
+    	online = false;
+    	
         if(readingThread != null) {
             readingThread.interrupt();
             readingThread = null;
         }
 
-        if(socket != null && socket.isClosed()){
-            socket = null;
-            dout = null;
-            return;
+        if(socket != null){
+        	try {
+				socket.close();
+			} catch (IOException e) {}
         }
-        if(dout != null) {
-            try {
-                dout.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            socket = null;
-            dout = null;
-        }
+        
+        socket = null;
+        dout = null;
+        dis = null;
     }
 
     public static boolean isClosed() {
