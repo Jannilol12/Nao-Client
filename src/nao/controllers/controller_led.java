d7352a6376db77983e2cb08f2431caf5ea1493ef
Jannilol12 <Jansko76@gmx.de>
2019-07-11 21:50:40 +0200

    RGb Update, Bug Fix

--- src/nao/controllers/controller_led.java
+++ src/nao/controllers/controller_led.java
@@ -8,6 +8,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import nao.sender;
 
+import java.awt.image.ColorConvertOp;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -40,9 +41,18 @@ public class controller_led implements Initializable {
     @FXML
     private ComboBox<String> Fuss_Rechts;
 
-
     @FXML
     private ComboBox<String> method;
+
+    @FXML
+    private TextField random_duration;
+
+    @FXML
+    private TextField rotate_rotation;
+
+    @FXML
+    private TextField rotate_duration;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -122,65 +132,74 @@ public class controller_led implements Initializable {
     @FXML
     void Start_btn(ActionEvent event) {
         try {
-            if (Gehirn.getSelectionModel().getSelectedItem() != null) {
-                if (Gehirn.getSelectionModel().getSelectedItem() == "Alle") {
-                    for (int i = 0; i < 12; i++) {
-                        sender.sendMessage("{\"type\":\"leds\",\"method\": \"" + method.getSelectionModel().getSelectedItem() + "\", \"ledname\": \"Brain" + i + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-                    }
-                } else
-                    sender.sendMessage("{\"type\":\"leds\",\"method\": \"" + method.getSelectionModel().getSelectedItem() + "\", \"ledname\": \"Brain" + Gehirn.getSelectionModel().getSelectedItem() + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-            }
-            if (Ohr_Links.getSelectionModel().getSelectedItem() != null) {
-                if (Ohr_Links.getSelectionModel().getSelectedItem() == "Alle") {
-                    for (int i = 1; i < 11; i++) {
-                        sender.sendMessage("{\"type\":\"leds\", \"method\":" + method.getSelectionModel().getSelectedItem() + ",\"ledname\": \"LeftEarLed" + i + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-                    }
-                } else
-                    sender.sendMessage("{\"type\":\"leds\", \"method\":" + method.getSelectionModel().getSelectedItem() + ",\"ledname\": \"LeftEarLed" + Ohr_Links.getSelectionModel().getSelectedItem() + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-            }
-            if (Ohr_Rechts.getSelectionModel().getSelectedItem() != null) {
-                if (Ohr_Rechts.getSelectionModel().getSelectedItem() == "Alle") {
-                    for (int i = 1; i < 11; i++) {
-                        sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"RightEarLed" + i + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-                    }
-                } else
-                    sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"RightEarLed" + Ohr_Rechts.getSelectionModel().getSelectedItem() + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-            }
-            if (Auge_Links.getSelectionModel().getSelectedItem() != null) {
-                if (Auge_Links.getSelectionModel().getSelectedItem() == "Alle") {
-                    for (int i = 1; i < 9; i++) {
-                        sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"LeftFaceLed" + i + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-                    }
-                } else
-                    sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"LeftFaceLed" + Auge_Links.getSelectionModel().getSelectedItem() + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-            }
-            if (Auge_Rechts.getSelectionModel().getSelectedItem() != null) {
-                if (Auge_Rechts.getSelectionModel().getSelectedItem() == "Alle") {
-                    for (int i = 1; i < 9; i++) {
-                        sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"RightFaceLed" + i + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-                    }
+            if(!random_duration.getText().isEmpty()){
+                sender.sendMessage("{\"type\":\"leds\",\"method\": \"random\", \"duration\":" + Float.parseFloat(random_duration.getText())+ "}");
+            }else if(!rotate_duration.getText().isEmpty() && !rotate_rotation.getText().isEmpty()){
+                int rgb = (int) Colour.getValue().getRed() << 8*2 + (int) Colour.getValue().getGreen() << 8 + (int) Colour.getValue().getBlue();
+                sender.sendMessage("{\"type\":\"leds\",\"method\": \"rotate\", \"rgbe\":" + rgb + ", \"timeR\":" + Float.parseFloat(rotate_rotation.getText()) + ", \"timeD\":" + Float.parseFloat(rotate_rotation.getText()) + "}");
+            }else {
+                if (Gehirn.getSelectionModel().getSelectedItem() != null) {
+                    if (Gehirn.getSelectionModel().getSelectedItem().equalsIgnoreCase("Alle")) {
+                        for (int i = 0; i < 12; i++) {
+                            sender.sendMessage("{\"type\":\"leds\",\"method\": \"" + method.getSelectionModel().getSelectedItem() + "\", \"ledname\": \"Brain" + i + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                        }
+                    } else
+                        sender.sendMessage("{\"type\":\"leds\",\"method\": \"" + method.getSelectionModel().getSelectedItem() + "\", \"ledname\": \"Brain" + Gehirn.getSelectionModel().getSelectedItem() + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
                 }
-                sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"RightFaceLed" + Auge_Rechts.getSelectionModel().getSelectedItem() + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-            }
-            if (Fuss_Links.getSelectionModel().getSelectedItem() != null) {
-                if (Fuss_Links.getSelectionModel().getSelectedItem() == "Alle") {
-                    sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"LeftFootLedsBlue\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-                    sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"LeftFootLedsGreen\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-                    sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"LeftFootLedsRed\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-                } else
-                    sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"LeftFootLeds" + Fuss_Links.getSelectionModel().getSelectedItem() + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-            }
-            if (Fuss_Rechts.getSelectionModel().getSelectedItem() != null) {
-                if (Fuss_Rechts.getSelectionModel().getSelectedItem() == "Alle") {
-                    sender.sendMessage("{\"type\":\"leds\", \"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\":\"RightFootLedsBlue\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-                    sender.sendMessage("{\"type\":\"leds\", \"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\":\"RightFootLedsGreen\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
-                    sender.sendMessage("{\"type\":\"leds\", \"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\":\"RightFootLedsRed\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText())+ "}");
+                if (Ohr_Links.getSelectionModel().getSelectedItem() != null) {
+                    if (Ohr_Links.getSelectionModel().getSelectedItem().equalsIgnoreCase("Alle")) {
+                        for (int i = 1; i < 11; i++) {
+                            sender.sendMessage("{\"type\":\"leds\", \"method\":" + method.getSelectionModel().getSelectedItem() + ",\"ledname\": \"LeftEarLed" + i + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                        }
+                    } else
+                        sender.sendMessage("{\"type\":\"leds\", \"method\":" + method.getSelectionModel().getSelectedItem() + ",\"ledname\": \"LeftEarLed" + Ohr_Links.getSelectionModel().getSelectedItem() + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
                 }
-                sender.sendMessage("{\"type\":\"leds\", \"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\":\"RightFootLeds" + Fuss_Rechts.getSelectionModel().getSelectedItem() + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                if (Ohr_Rechts.getSelectionModel().getSelectedItem() != null) {
+                    if (Ohr_Rechts.getSelectionModel().getSelectedItem().equalsIgnoreCase("Alle")) {
+                        for (int i = 1; i < 11; i++) {
+                            sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"RightEarLed" + i + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                        }
+                    } else
+                        sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"RightEarLed" + Ohr_Rechts.getSelectionModel().getSelectedItem() + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                }
+                if (Auge_Links.getSelectionModel().getSelectedItem() != null) {
+                    if (Auge_Links.getSelectionModel().getSelectedItem().equalsIgnoreCase("Alle")) {
+                        for (int i = 1; i < 9; i++) {
+                            sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"LeftFaceLed" + i + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                        }
+                    } else
+                        sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"LeftFaceLed" + Auge_Links.getSelectionModel().getSelectedItem() + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                }
+                if (Auge_Rechts.getSelectionModel().getSelectedItem() != null) {
+                    if (Auge_Rechts.getSelectionModel().getSelectedItem().equalsIgnoreCase("Alle")) {
+                        for (int i = 1; i < 9; i++) {
+                            sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"RightFaceLed" + i + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                        }
+                    }
+                    sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"RightFaceLed" + Auge_Rechts.getSelectionModel().getSelectedItem() + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                }
+                if (Fuss_Links.getSelectionModel().getSelectedItem() != null) {
+                    if (Fuss_Links.getSelectionModel().getSelectedItem().equalsIgnoreCase("Alle")) {
+                        sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"LeftFootLedsBlue\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                        sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"LeftFootLedsGreen\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                        sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"LeftFootLedsRed\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                    } else
+                        sender.sendMessage("{\"type\":\"leds\",\"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\": \"LeftFootLeds" + Fuss_Links.getSelectionModel().getSelectedItem() + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                }
+                if (Fuss_Rechts.getSelectionModel().getSelectedItem() != null) {
+                    if (Fuss_Rechts.getSelectionModel().getSelectedItem().equalsIgnoreCase("Alle")) {
+                        sender.sendMessage("{\"type\":\"leds\", \"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\":\"RightFootLedsBlue\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                        sender.sendMessage("{\"type\":\"leds\", \"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\":\"RightFootLedsGreen\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                        sender.sendMessage("{\"type\":\"leds\", \"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\":\"RightFootLedsRed\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                    }
+                    sender.sendMessage("{\"type\":\"leds\", \"method\":" + method.getSelectionModel().getSelectedItem() + ", \"ledname\":\"RightFootLeds" + Fuss_Rechts.getSelectionModel().getSelectedItem() + "\", \"red\":" + (float) Colour.getValue().getRed() + ", \"green\":" + Colour.getValue().getGreen() + ", \"blue\":" + Colour.getValue().getBlue() + ", \"Fade\":" + Float.parseFloat(Fade.getText()) + "}");
+                }
             }
         }catch (NumberFormatException err){
             System.out.println("NumberFormatException");
+            err.printStackTrace();
         }
+
         Gehirn.getSelectionModel().clearSelection();
         Gehirn.setPromptText("Gehirn");
         Ohr_Rechts.getSelectionModel().clearSelection();
