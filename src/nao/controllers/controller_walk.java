ce869de0c3d495d92db2298270d5af900d825b2c
BuildTools <unconfigured@null.spigotmc.org>
2019-07-12 19:42:52 +0200

    updated

--- src/nao/controllers/controller_walk.java
+++ src/nao/controllers/controller_walk.java
@@ -1,16 +1,15 @@
 package nao.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import nao.sender;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class controller_walk implements Initializable {
 	@FXML
