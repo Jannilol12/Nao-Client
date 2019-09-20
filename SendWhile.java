30b979433673ae0e5f24628ce1f240bc5d0cb27a
Jannilol12 <52676250+Jannilol12@users.noreply.github.com>
2019-09-20 12:16:40 +0200

    Volume

---
+++ SendWhile.java
@@ -0,0 +1,69 @@
+package nao;
+
+import components.json.JSONObject;
+
+public class SendWhile {
+    private Thread t;
+    private Thread a;
+
+    public synchronized void sendAudioPlayer(){
+        if(a != null){
+            return;
+        }
+        a = new Thread(){
+            @Override
+            public void run() {
+                while (!this.isInterrupted() && !sender.isClosed()) {
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.add("type", "audioPlayer");
+                    jsonObject.add("function", "getPosition");
+                    sender.sendMessage(jsonObject.toJSONString());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {}
+                }
+                a = null;
+            }
+        };
+        a.start();
+    }
+
+    public synchronized void stopAudioPlayer(){
+        if(a == null) return;
+
+        a.interrupt();
+        a = null;
+    }
+
+    public synchronized void sendVolume(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.add("type", "audioPlayer");
+        jsonObject.add("function", "getVolume");
+        sender.sendMessage(jsonObject.toJSONString());
+    }
+
+    public synchronized void sendBattery(){
+        if(t != null){
+            return;
+        }
+        t = new Thread(){
+            @Override
+            public void run() {
+                while (!this.isInterrupted() && !sender.isClosed()) {
+                    sender.sendMessage("{\"type\":\"battery\"}");
+                    try {
+                        Thread.sleep(30000);
+                    } catch (InterruptedException e) {}
+                }
+                t = null;
+            }
+        };
+        t.start();
+    }
+    public synchronized void stopBattery(){
+    	if(t == null) return;
+    	
+        t.interrupt();
+        t = null;
+    }
+}
