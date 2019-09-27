fd31613e0da385277748e4dd27d41b695885ce8d
Jannilol12 <Jansko76@gmx.de>
2019-09-27 18:08:27 +0200

    Events

--- src/nao/controllers/controller_Events.java
+++ src/nao/controllers/controller_Events.java
@@ -9,10 +9,13 @@ import nao.sender;
 
 public class controller_Events {
     private boolean isFeetContactButtonSelected = false;
+    private boolean isSpeechRecognitionButtonSelected = false;
 
 
     @FXML
     private ToggleButton FeetContactButton;
+    @FXML
+    private ToggleButton SpeechRecognitionButton;
 
 
     @FXML
@@ -36,7 +39,28 @@ public class controller_Events {
             jsonObject.add("boolean",  "true");
             sender.sendMessage(jsonObject.toJSONString());
         }
+    }
 
+    @FXML
+    void SpeechRecognition(ActionEvent event) {
+        if(isSpeechRecognitionButtonSelected){
+            SpeechRecognitionButton.setStyle("-fx-background-color: Red");
+            isSpeechRecognitionButtonSelected = false;
 
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.add("type", "Events");
+            jsonObject.add("function", "SpeechRecognition");
+            jsonObject.add("boolean",  "false");
+            sender.sendMessage(jsonObject.toJSONString());
+        } else{
+            SpeechRecognitionButton.setStyle("-fx-background-color: Green");
+            isSpeechRecognitionButtonSelected = true;
+
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.add("type", "Events");
+            jsonObject.add("function", "SpeechRecognition");
+            jsonObject.add("boolean",  "true");
+            sender.sendMessage(jsonObject.toJSONString());
+        }
     }
 }
